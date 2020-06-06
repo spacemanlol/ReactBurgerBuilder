fcf3f9ac637bf5dab4cbe690097766ae862d034d
spacemanlol <shafinschowdhury@gmail.com>
2020-06-05 19:19:55 -0500

    Creation of buildControl object, css styling

--- burger-builder-starting-setup/src/components/Burger/Burger.js
+++ burger-builder-starting-setup/src/components/Burger/Burger.js
@@ -15,7 +15,7 @@ const burger = (props) => {
             return arr.concat(el);
         }, []);
 
-    // Ask user to start adding ingredients if none have been added so far 
+    // Ask user to start adding ingredients if none have been added so far
     if(transformedIngredients.length === 0){
         transformedIngredients = <p>Please start adding ingredients!</p>;
     }
