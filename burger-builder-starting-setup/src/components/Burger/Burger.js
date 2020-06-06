34cf496fcfdc05a32a16649679057b77e63a20f8
spacemanlol <shafinschowdhury@gmail.com>
2020-06-05 19:02:45 -0500

    Addition of State and semi dynamic output
    Burger now outputs ingredients based on state of Burger
    
    added function which takes an object array and converts it to an array of ingredients

--- burger-builder-starting-setup/src/components/Burger/Burger.js
+++ burger-builder-starting-setup/src/components/Burger/Burger.js
@@ -4,12 +4,19 @@ import BurgerIngredient from "./BurgerIngredients/BurgerIngredients";
 import BurgerBuilder from "../../containers/BurgerBuilder/BurgerBuilder";
 
 const burger = (props) => {
+    // Convert ingredient object to an array
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey+i} type={igKey} />
+            });
+        });
+
     return (
         // Wrapping to give styling
             <div className={classes.Burger}>
                 <BurgerIngredient type="bread-top"/>
-                <BurgerIngredient type="cheese"/>
-                <BurgerIngredient type="meat"/>
+                {transformedIngredients}
                 <BurgerIngredient type="bread-bottom"/>
             </div>
     );
